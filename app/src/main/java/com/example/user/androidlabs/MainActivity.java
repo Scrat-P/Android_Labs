19bd2a7aeebf555eb71e774a5fbe983c83bf7613
Pavel Baranovich <baranovich.pavel@yandex.ru>
2018-12-05 17:41:16 +0300

    Fixed working with git tags

--- app/src/main/java/com/example/user/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/user/androidlabs/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else {
             requestPermissions();
+            shouldShowPermissionExplanation = true;
         }
     }
 
